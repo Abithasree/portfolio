be0924ee5645d12a4b25f67c8b095ccd932cf3c7
Kolli Abithasri <abithasreekolli@gmail.com>
2024-02-10 17:41:07 +0530

    hurrying

---
+++ index.js
@@ -0,0 +1,9 @@
+//nav bar
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+//scrolling should hide the nav so that code here
+
